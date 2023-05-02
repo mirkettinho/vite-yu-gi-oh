1d1cf29fbc77352de2da7f6303e977d017560c3a
mirkettinho <mirkodifranco30@gmail.com>
2023-05-02 19:04:32 +0200

    [add] header

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<template>
+  <header>
+    <div class="vertical">
+      <img src="../assets/img/pngegg.png" alt="logo ">
+      <span>Yu-Gi-Oh Api</span>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+
+header{
+  height: 40px;
+  background-color: white;
+  display: flex;
+  padding-left: 10px;
+}.vertical{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}span{
+  color: black;
+  padding-left: 10px;
+}
+
+</style>
